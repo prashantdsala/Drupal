83772b30918c6191d4f0002743261adb60563cda
prashantdsala <p1989chauhan@gmail.com>
2024-07-16 16:46:30 +0530

    Adding gen files

---
+++ _site/composer_files/README.md
@@ -0,0 +1,25 @@
+CONTENTS OF THIS FILE
+---------------------
+ * Introduction
+ * Author
+
+INTRODUCTION
+-----------
+
+ * Contains example composer files for drupal module, theme, profile and library.
+
+
+AUTHOR
+------------------
+Author:
+ * Prashant Chauhan(Prashant.c) - https://www.drupal.org/u/prashantc
+
+Github profile:
+ * https://github.com/prashantdsala
+
+Github Drupal Repository: 
+ * https://github.com/prashantdsala/Drupal
+
+LinkedIn profile
+ * https://in.linkedin.com/in/prashant-chauhan-dharamshala
+ 
---
+++ _site/modules/custom_render_element/src/Element/Entity.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\custom_render_element\Element;
+
+use Drupal\Core\Render\Element\RenderElement;
+
+/**
+ * Provides a render element to display an entity.
+ *
+ * Properties:
+ * - #entity_type: The entity type.
+ * - #entity_id: The entity ID.
+ * - #view_mode: The view mode that should be used to render the entity.
+ * - #langcode: For which language the entity should be rendered.
+ *
+ * Usage Example:
+ * @code
+ * $build['node'] = [
+ *   '#type' => 'entity',
+ *   '#entity_type' => 'node',
+ *   '#entity_id' => 1,
+ *   '#view_mode' => 'teaser,
+ *   '#langcode' => 'en',
+ * ];
+ * @endcode
+ *
+ * @RenderElement("entity")
+ */
+class Entity extends RenderElement {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getInfo() {
+    return [
+      '#pre_render' => [
+        [get_class($this), 'preRenderEntityElement'],
+      ],
+      '#view_mode' => 'full',
+      '#langcode' => NULL,
+    ];
+  }
+
+  /**
+   * Entity element pre render callback.
+   *
+   * @param array $element
+   *   An associative array containing the properties of the entity element.
+   *
+   * @return array
+   *   The modified element.
+   */
+  public static function preRenderEntityElement(array $element) {
+
+    $entity_type_manager = \Drupal::entityTypeManager();
+    // Load the entity provided in the element.
+    $entity = $entity_type_manager
+      ->getStorage($element['#entity_type'])
+      ->load($element['#entity_id']);
+
+    if ($entity && $entity->access('view')) {
+      // Render entity with entity view builder see core/lib/Drupal/Core/Entity/EntityTypeManager.php.
+      $element['entity'] = $entity_type_manager
+        ->getViewBuilder($element['#entity_type'])
+        ->view($entity, $element['#view_mode'], $element['#langcode']);
+    }
+
+    return $element;
+  }
+
+}
